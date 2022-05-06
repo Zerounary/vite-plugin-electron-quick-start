9e7cc145eab71722c603f5984016c4c0ae971416
Zerounary <850657663@qq.com>
2022-05-06 14:50:35 +0800

    test(pinia test): add pinia smoke test

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -1,7 +1,19 @@
-import { expect, describe, it } from "vitest"
+import { beforeEach, expect, describe, it } from "vitest"
+import { setActivePinia, createPinia } from 'pinia';
+import { useCounterStore } from '../src/stores/app';
+
+beforeEach(() => {
+  setActivePinia(createPinia());
+})
 
 describe("smoke test" , () => {
   it("should be able to add", () => {
     expect(1 + 1).toEqual(2)
+  })
+  it("pinia count test", () => {
+    const store = useCounterStore();
+    expect(store.count).toEqual(0)
+    store.increment();
+    expect(store.count).toEqual(1)
   })
 })
