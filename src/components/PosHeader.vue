f12a33ff738203e8847ab0e02c4bb1de2c8cd6a5
Zerounary <850657663@qq.com>
2022-05-16 15:16:59 +0800

    feat(psoheader): 完善postheader

--- src/components/PosHeader.vue
+++ src/components/PosHeader.vue
@@ -1,13 +1,18 @@
 <template>
-  <div class="h-50px shadow-xl bg-white">
-    
+  <div class="grid grid-cols-2 items-center h-50px shadow-xl bg-white">
+    <div class="text-left flex items-center ">
+      <Icon icon="material-symbols:store-outline" width="24" height="24" />
+      <div>门店管理</div>
+    </div>
+    <div class="flex items-center justify-end ">
+      <Icon icon="bxs:user" width="24" height="24" />
+      <div>系统管理员</div>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
-
+import { Icon } from "@iconify/vue";
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
