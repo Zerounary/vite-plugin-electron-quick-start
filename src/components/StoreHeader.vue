0c622f93a94a467dd1936f37767d9b02a0b64863
Zerounary <850657663@qq.com>
2022-05-17 16:23:35 +0800

    调整 StoreHeader 样式

--- src/components/StoreHeader.vue
+++ src/components/StoreHeader.vue
@@ -1,12 +1,12 @@
 <template>
   <div>
-    <div class="grid grid-cols-2 items-center h-50px shadow bg-blue-500 text-white px-3">
-      <div class="text-left flex items-center ">
-        <div class="text-2xl">{{storeStore.name}}</div>
+    <div class="grid grid-cols-2 items-center h-50px shadow bg-white h-60px px-3">
+      <div class="text-left flex items-center space-x-6 ">
+        <div class="text-xl font-bold">{{storeStore.name}}</div>
       </div>
       <div class="flex items-center justify-end space-x-3 ">
-        <div>库存总量：{{storeStore.storageQty}}</div>
-        <div>会员总数：{{storeStore.vipNum}}</div>
+        <div class="store-sum-colls" >库存总量：{{storeStore.storageQty}}</div>
+        <div class="store-sum-colls" >会员总数：{{storeStore.vipNum}}</div>
       </div>
     </div>
   </div>
@@ -20,5 +20,9 @@ const storeStore = userStoreStore();
 </script>
 
 <style scoped>
+.store-sum-cols {
+  @apply
+    text-gray-500;
+}
 
 </style>
