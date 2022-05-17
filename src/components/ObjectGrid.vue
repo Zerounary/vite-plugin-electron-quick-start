2101821daddd50f7884bff49b6e05edf17651684
Zerounary <850657663@qq.com>
2022-05-17 16:18:49 +0800

    调整 ObjectGrid 的样式

--- src/components/ObjectGrid.vue
+++ src/components/ObjectGrid.vue
@@ -1,8 +1,8 @@
 <template>
-  <div class="flex space-x-2">
+  <div class="flex items-center space-x-2 bg-white rounded shadow h-100px">
     <div class="box" v-for="cell in cells" :key="cell.title">
-      <div class="font-bold text-16px h-30px leading-30px">{{cell.title}}</div>
-      <div class="text-red-500 h-20px leading-20px">{{cell.value}}</div>
+      <div class="font-bold text-16px text-gray-600 h-30px leading-30px">{{cell.title}}</div>
+      <div class="text-red-500 font-bold h-20px leading-20px">{{cell.value}}</div>
     </div>
   </div>
 </template>
@@ -32,6 +32,6 @@ let cells = computed(() => {
 <style scoped>
 .box {
   @apply
-    px-2 py-1 border rounded bg-white text-center shadow w-min-100px space-y-1
+    px-2 py-1 border rounded bg-white text-center w-min-100px space-y-1
 }
 </style>
