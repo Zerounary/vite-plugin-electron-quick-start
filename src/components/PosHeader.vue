e04c8111fa7b663593b5d06de9de0c05c5065deb
Zerounary <850657663@qq.com>
2022-05-17 16:30:28 +0800

    添加退出按钮

--- src/components/PosHeader.vue
+++ src/components/PosHeader.vue
@@ -1,18 +1,33 @@
 <template>
   <div class="grid grid-cols-2 items-center h-50px shadow bg-white px-3">
-    <div class="text-left flex items-center ">
+    <div class="text-left flex items-center">
       <Icon icon="material-symbols:store-outline" width="24" height="24" />
       <div>门店管理</div>
     </div>
-    <div class="flex items-center justify-end ">
+    <div class="flex items-center justify-end">
       <Icon icon="bxs:user" width="24" height="24" />
       <div>系统管理员</div>
+      <Icon
+        icon="ant-design:logout-outlined"
+        class="cursor-pointer"
+        title="点击退出"
+        color="#ef4444"
+        width="24"
+        height="24"
+        @click="quit"
+      />
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
 import { Icon } from "@iconify/vue";
+import { rendererSend } from '~/electron-main/common/ipcRender'
+import ipcNames from '~/electron-main/common/ipcNames';
+
+let quit = () => {
+  rendererSend(ipcNames.quit, null)
+}
 </script>
 
 <style scoped></style>
