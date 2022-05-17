ad0f851d1df862ceeab2eec0af70e2b3088c2bed
Zerounary <850657663@qq.com>
2022-05-17 15:21:51 +0800

    添加，排行组件

---
+++ src/components/RankList.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="w-min-300px bg-white h-400px p-3 rounded shadow-xl space-y-3">
+    <div class="font-bold">{{title}}</div>
+    <table class="w-full">
+      <thead class="w-full">
+        <template v-for="head in heads" :key="head">
+          <th class="text-gray-500 font-normal text-14px">
+            {{head.label}}
+          </th>
+        </template>
+      </thead>
+      <tbody class=" text-13px text-black text-center">
+        <template v-for="item in data" :key="item">
+          <tr class="h-30px">
+            <template v-for="head in heads" :key="head">
+              <td>
+                  {{item[head.prop]}}
+              </td>
+            </template>     
+          </tr>
+        </template>
+      </tbody>
+    </table>
+    <div class="h-30px text-14px text-blue-500 font-bold text-center">
+      查看更多
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+interface Head {
+
+}
+interface Item {
+
+}
+interface Props {
+  title: string
+  heads: Head[]
+  data: Item[]
+}
+const props = withDefaults(defineProps<Props>(), {
+  title: '',
+  heads: () => [],
+  data: () => []
+})
+
+</script>
+
+<style scoped>
+
+</style>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -8,6 +8,12 @@
     <div class="center">
       <ObjectGrid :value="grid" />
     </div>
+
+    <div class="columns-3">
+      <RankList title="商品销售排行" :heads="heads" :data="data" />
+      <RankList title="导购业绩" />
+      <RankList title="会员销售排行" />
+    </div>
   </div>
 </template>
 
@@ -15,24 +21,55 @@
 import StoreHeader from "../components/StoreHeader.vue";
 import DateNav from "../components/DateNav.vue";
 import ObjectGrid from "../components/ObjectGrid.vue";
+import RankList from "../components/RankList.vue";
 
 const grid = {
-  '零售额': 2819,
-  '销售指标': 2819,
-  '达成率': '20%',
-  '零售数量': 2819,
-  '退货数量': 2819,
-  '退货率': '10%',
-  '客单价': 100,
-  '连带率': 2.3,
-  '会销率': '89%',
-  '新增会员': 2819,
+  零售额: 2819,
+  销售指标: 2819,
+  达成率: "20%",
+  零售数量: 2819,
+  退货数量: 2819,
+  退货率: "10%",
+  客单价: 100,
+  连带率: 2.3,
+  会销率: "89%",
+  新增会员: 2819,
+};
 
+const heads = [
+  {
+    label: "商品",
+    prop: "name",
+  },
+  {
+    label: "商品",
+    prop: "dayAvgQty",
+  },
+  {
+    label: "可用库存",
+    prop: "storageQty",
+  },
+  {
+    label: "可售天数",
+    prop: "saleDay",
+  },
+];
+
+let item = {
+  name: "商品1",
+  dayAvgQty: 2819,
+  storageQty: 2819,
+  saleDay: 28,
+};
+const data = [];
+
+for (let i = 0; i < 4; i++) {
+  data.push(item);
 }
 </script>
 
 <style scoped>
-  .center {
-    @apply w-full flex justify-center;
-  }
+.center {
+  @apply w-full flex justify-center;
+}
 </style>
