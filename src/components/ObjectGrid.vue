7a926f192cfa214c07599ad352d78ea5cd6c7119
Zerounary <850657663@qq.com>
2022-05-18 14:58:29 +0800

    添加 @apply 的示例

--- src/components/ObjectGrid.vue
+++ src/components/ObjectGrid.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="flex items-center space-x-2 bg-white rounded shadow h-100px">
-    <div class="px-2 py-1 border rounded bg-white text-center w-min-100px space-y-1" v-for="cell in cells" :key="cell.title">
+    <div class="box" v-for="cell in cells" :key="cell.title">
       <div class="font-bold text-16px text-gray-600 h-30px leading-30px">{{cell.title}}</div>
       <div class="text-red-500 font-bold h-20px leading-20px">{{cell.value}}</div>
     </div>
@@ -30,5 +30,7 @@ let cells = computed(() => {
 </script>
 
 <style scoped>
-
+.box{
+  @apply px-2 py-1 border rounded bg-white text-center w-min-100px space-y-1;
+}
 </style>
