4ff40b5ecb9c2e89202ccb6f5fcf4d51f45f7acc
Zerounary <850657663@qq.com>
2022-06-06 14:09:30 +0800

    添加缓存加载界面

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,22 +1,26 @@
 <template>
-  <div>
-    <router-link to="/pos/home">/pos/home</router-link>
-    <button @click="insertIt">插入</button>
-    {{cache.isLoading}}
-    {{cache.loadingMessage}}
+  <div class="w-screen h-screen flex items-center justify-center">
+    <div class=" w-300px h-200px pt-30px text-center text-xl">{{ cache.loadingMessage }}</div>
   </div>
 </template>
 
 <script setup lang="ts">
-import { onMounted } from 'vue';
-import { useCacheStore } from '@/stores/cache';
+import { onMounted } from "vue";
+import { ElLoading } from "element-plus";
+import { useCacheStore } from "@/stores/cache";
 
 const cache = useCacheStore();
 
 onMounted(async () => {
+  const loading = ElLoading.service({
+    fullscreen: true,
+    lock: true,
+    background: "rgba(0, 0, 0, 0.7)",
+  });
   await cache.cacheAll();
+  loading.close();
   router.push("/pos/home");
-})
+});
 </script>
 
 <style scoped></style>
